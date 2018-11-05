0e932453ff51de65934d251e8816e1177bd80a08
Dan Doan <doan_dan@outlook.com>
2018-11-05 10:31:13 -0800

    made adjustments

--- buddhistyouthgroup/buddhistyouthgroup/ClientApp/src/app/Seattle-GDPTLieuQuan/home/home.component.ts
+++ buddhistyouthgroup/buddhistyouthgroup/ClientApp/src/app/Seattle-GDPTLieuQuan/home/home.component.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class SeattleGDPTLieuQuanHomeComponent implements OnInit {
 
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://picsum.photos/1000/500?random&t=${Math.random()}`);
   obj: any = { title: 'No Sinh Hoat: Huynh Truong Conference', start: '2018-10-28' };
   eventsArray: any[];
 
